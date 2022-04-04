b0729cc5572f816a57bfb680295ce4c40e00fbab
Joshua Farnaby <joshuafarnabyuk@gmail.com>
2022-04-04 11:01:06 +0100

    password tested against regex and each other

--- script.js
+++ script.js
@@ -4,33 +4,69 @@ const APP = (() => {
   const firstNameControl = document.getElementById('first-name');
   const lastNameControl = document.getElementById('last-name');
   const emailControl = document.getElementById('email');
+  const numberControl = document.getElementById('number');
+  const passwordControl = document.getElementById('password');
+  const passwordControl2 = document.getElementById('password2');
 
   const validateName = function () {
     let formControl = this;
     let nameType = formControl.name == 'first-name' ? 'first' : 'last';
-    let errorContainer = formControl.parentElement.querySelector('span.error-msg');
 
     if (formControl.validity.valid) {
-      removeError(errorContainer);
+      removeError(formControl);
     } else if (formControl.validity.valueMissing) {
-      showError(errorContainer, `Please enter a ${nameType} name`);
+      showError(formControl, `Please enter a ${nameType} name`);
     }
   }
 
   const validateEmail = function () {
-    const errorContainer = emailControl.parentElement.querySelector('span.error-msg');
-
     if (emailControl.validity.valid) {
-      removeError(errorContainer)
+      removeError(emailControl)
     } else if (emailControl.validity.typeMismatch || emailControl.validity.valueMissing) {
-      showError(errorContainer, 'Please enter an email address in format a@b')
+      showError(emailControl, 'Please enter an email address in format a@b')
     }
   }
 
-  const removeError = (element) => element.textContent = '';
-  const showError = (element, msg) => element.textContent = msg;
+  const validateNumber = function () {
+    const values = numberControl.value.split('');
+    const lastValue = values[values.length - 1];
+
+    if (numberControl.validity.valueMissing) {
+      showError(numberControl, 'Please enter a number between 8 and 12 digits')
+    } else if (!/[0-9]/.test(lastValue)) {
+      showError(numberControl, 'Please only include digits 0-9')
+    } else if (numberControl.validity.tooShort || numberControl.validity.tooShort) {
+      showError(numberControl, 'Please enter a number between 8 and 12 digits')
+    } else if (numberControl.validity.valid) {
+      removeError(numberControl)
+    }
+  }
+
+  const validatePassword = function () {
+    const pattern = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
+    if (!pattern.test(this.value)) {
+      showError(this, '8 characters with 1 letter and 1 number')
+    } else {
+      removeError(this);
+    }
+
+    if (this.id == 'password2') {
+      if (this.value != passwordControl.value) {
+        showError(this, 'Passwords do not match')
+      } else {
+        removeError(this)
+      }
+    }
+  }
+
+  const removeError = (formControl) => formControl.parentElement.querySelector('span.error-msg').textContent = '';
+  const showError = (formControl, msg) => formControl.parentElement.querySelector('span.error-msg').textContent = msg;
 
   firstNameControl.addEventListener('input', validateName.bind(firstNameControl));
   lastNameControl.addEventListener('input', validateName.bind(lastNameControl));
-  emailControl.addEventListener('change', validateEmail);
+  emailControl.addEventListener('input', validateEmail);
+  numberControl.addEventListener('input', validateNumber);
+  passwordControl.addEventListener('input', validatePassword.bind(passwordControl));
+  passwordControl2.addEventListener('input', validatePassword.bind(passwordControl2));
 })();

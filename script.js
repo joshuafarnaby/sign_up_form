d7b1f5a6424f6332fb6b1d65d64df135ffe3db57
Joshua Farnaby <joshuafarnabyuk@gmail.com>
2022-04-04 09:42:39 +0100

    error msg will display if first/last name contorl is left empty

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-const signUpForm = document.getElementById('sign-up-form');
+const APP = (() => {
+  const signUpForm = document.getElementById('sign-up-form');
 
-const firstNameControl = document.getElementById('first-name');
-const firstNameError = document.getElementById('first-name-error');
+  const firstNameControl = document.getElementById('first-name');
+  const lastNameControl = document.getElementById('last-name');
 
-firstNameControl.addEventListener('input', (ev) => {
-  console.log(firstNameControl.validity);
+  const validateName = function () {
+    let formControl = this;
+    let nameType = formControl.name == 'first-name' ? 'first' : 'last';
+    let errorContainer = formControl.parentElement.querySelector('span.error-msg');
 
-  // if (firstNameControl.validity.valid) {
-  //   firstNameError.textContent = '';
-  // } else {
-  //   if (firstNameControl.validity.valueMissing) {
-  //     firstNameError.textContent = 'You must enter a first name'
-  //   }
-  // }
-})
+    if (formControl.validity.valid) {
+      removeError(errorContainer);
+    } else if (formControl.validity.valueMissing) {
+      showError(errorContainer, `A ${nameType} name is required`);
+    }
+  }
+
+  const removeError = (element) => element.textContent = '';
+  const showError = (element, msg) => element.textContent = msg;
+
+  firstNameControl.addEventListener('input', validateName.bind(firstNameControl));
+  lastNameControl.addEventListener('input', validateName.bind(lastNameControl));
+})();
